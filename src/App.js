ec1637947103f229d37b1462e46560e8a803ac69
Belospea <bb8595264@gmail.com>
2019-12-24 11:54:21 -0700

    array manage, remove update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
 import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
-import PhoneInfoList from './components/PhoneInfoList';
+import PhoneInfoList from "./components/PhoneInfoList";
 
 class App extends Component {
-  id = 2
+  id = 2;
   state = {
     information: [
       {
         id: 0,
-        name: 'Josh',
-        phone: '1-355-654-8745'
+        name: "Josh",
+        phone: "1-355-654-8745"
       },
       {
         id: 1,
-        name: 'Mark',
-        phone: '1-654-578-6145'
+        name: "Mark",
+        phone: "1-654-578-6145"
       }
     ]
-  }
+  };
 
   handleCreate = data => {
     const { information } = this.state;
     this.setState({
       information: information.concat({ id: this.id++, ...data })
-    })
+    });
   };
+
+  handleRemove = id => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    });
+  };
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id ? { ...info, ...data } : info
+      )
+    })
+  }
 
   render() {
     const { information } = this.state;
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
-        <PhoneInfoList data={this.state.information} />
+        <PhoneInfoList
+          data={information}
+          onRemove={this.handleRemove}
+          onUpdate ={this.handleUpdate}
+        />
       </div>
     );
   }
--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -8,6 +8,48 @@ class PhoneInfo extends Component {
       phone: "1231231231"
     }
   };
+
+  state = {
+    editing: false,
+    name: "",
+    phone: ""
+  };
+
+  handleRemove = () => {
+    const { info, onRemove } = this.props;
+    onRemove(info.id);
+  };
+
+  handleToogleEdit = () => {
+    const { editing } = this.state;
+    this.setState({ editing: !editing });
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { info, onUpdate } = this.props;
+
+    if(!prevState.editing && this.state.editing) {
+      this.setState({
+        name: info.name,
+        phone: info.phone
+      })
+    }
+
+    if(prevState.editing && !this.state.editing) {
+      onUpdate(info.id, {
+        name: this.state.name,
+        phone: this.state.phone
+      });
+    }
+  }
+
   render() {
     const style = {
       border: "1px solid black",
@@ -15,13 +57,42 @@ class PhoneInfo extends Component {
       margin: "8px"
     };
 
-    const { id, name, phone } = this.props.info;
+    const { editing } = this.state;
+
+    if (editing) {
+      return (
+        <div style={style}>
+          <div>
+            <input
+              value={this.state.name}
+              name="name"
+              placeholder="name"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              value={this.state.phone}
+              name="phone"
+              placeholder="phone number"
+              onChange={this.handleChange}
+            />
+          </div>
+          <button onClick={this.handleToogleEdit}>Apply</button>
+          <button onClick={this.handleRemove}>Remove</button>
+        </div>
+      );
+    }
+
+    const { name, phone } = this.props.info;
     return (
       <div style={style}>
         <div>
           <b>{name}</b>
         </div>
         <div>{phone}</div>
+        <button onClick={this.handleToogleEdit}>Edit</button>
+        <button onClick={this.handleRemove}>Remove</button>
       </div>
     );
   }
